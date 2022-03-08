97e5345a287430a1d680705699c381741af67b07
gp <tranhrp@gmail.com>
2022-03-08 23:45:15 +0700

    abc

---
+++ mang-doi-tuong/index.js
@@ -0,0 +1,48 @@
+// 1. Định nghĩa đối tượng iphone 5, iphone 6, iphone 7, iphone 8, iphone X, iphone 11, iphone 12 bao gồm các trường thông tin: hinhAnh, ten, gia
+let iphone5 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip5',
+    price: 20000000
+}
+
+let iphone6 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip6',
+    price: 20000000
+}
+
+let iphone7 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip7',
+    price: 20000000
+}
+
+let iphone8 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip8',
+    price: 20000000
+}
+
+let iphoneX = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ipX',
+    price: 20000000
+}
+
+let iphone11 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip11',
+    price: 20000000
+}
+
+let iphone12 = {
+    image: "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-vang-dong-new-600x600-1-600x600.jpg",
+    ten: 'ip12',
+    price: 20000000
+}
+
+// 2. Định nghĩa một mảng và thêm vào toàn bộ các đối tượng trên vào mảng 
+let arr = []
+
+arr.push(iphone5,iphone6,iphone7,iphone8,iphoneX,iphone11,iphone12)
+console.log(arr);
