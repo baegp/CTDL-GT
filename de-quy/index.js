5e2284872db61a6d4774e9cd2ced13b91a81633c
gp <tranhrp@gmail.com>
2022-03-19 17:40:57 +0700

    de quy

---
+++ de-quy/index.js
@@ -0,0 +1,15 @@
+const Finbonaci = (n) => {
+    let a = 1
+    let b = 0
+    let temp
+
+    for (a; n >= 0; n--) {
+        console.log(`finbonaci`, a);
+        temp = a;
+        a = a + b; 
+        b = temp;
+    }
+    return b;
+
+}
+Finbonaci(11)
